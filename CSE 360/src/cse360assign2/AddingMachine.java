f76be78a0a681519462a4cdd0e89611a2b5f4e82
ezilk <ezilk@DESKTOP-GNHF286>
2019-10-08 11:44:36 -0700

    Initial version of AddingMachine

---
+++ CSE 360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,50 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/*
+	 *Constructor method for creating an AddinaMachine object. 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * Get method which returns the AddingMachine object value, total.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * Method which takes an int value as a parameter, and adds that value to it's
+	 * AddingMachine object "total" value.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * Method which takes an int value as a parameter, and subtracts that value from
+	 * it's AddingMachine object "total" value. 
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/*
+	 * Method that returns the int value, total, as a string.
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/*
+	 * Method that clears the value from the AddingMachine object, total. 
+	 */
+	public void clear() {
+	
+	}
+}
